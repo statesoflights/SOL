6a42c4c5fa19e0156831a81f226cff3519d9751f
louhichio <louhichio@hotmail.com>
2015-03-24 18:05:21 +0100

    Update Interrupteur

---
+++ states_of_light/Assets/Scripts/Environment/Trigger_Interruptor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger_Interruptor : MonoBehaviour
+{
+    public SpriteRenderer sprite_Status;
+    public LightEmitter light_emitter;
+
+    public float activation_Time;
+    public int id;
+
+    void Start()
+    {
+        sprite_Status.color = Color.red;
+    }
+
+    IEnumerator Activate()
+    {
+        sprite_Status.color = Color.green;
+        light_emitter.InterruptorIsActive(id);
+
+        yield return new WaitForSeconds(activation_Time);
+
+        sprite_Status.color = Color.red;
+        light_emitter.InterruptorIsInactive(id);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Player") || other.tag.Equals("PlayerSmall"))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Activate());
+        }
+    }
+}
